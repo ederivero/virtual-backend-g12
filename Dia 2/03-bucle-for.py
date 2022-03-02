240b5ff51982e0bd7fed9a2fc766d1afe5e8b664
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-03-01 23:04:24 -0500

    avance for

--- Dia 2/03-bucle-for.py
+++ Dia 2/03-bucle-for.py
@@ -5,3 +5,28 @@ notas = [10, 20, 16, 8, 6, 1]
 # el mismo funcionamiento se da para cualquier coleccion de datos (lista, tupla, diccionario, conjunto)
 for nota in notas:
     print(nota)
+
+# crearemos un bucle manual para una iteracion hasta el limite definido en el range
+# https://docs.python.org/3/library/stdtypes.html?highlight=range#range
+for numero in range(10):
+    print(numero)
+
+# si colocamos dos parametros el primero significara el numero inicial y el segundo el tope
+for numero in range(5,10):
+    print(numero)
+
+# si colocamos dos parametros el primero significara el numero inicial, el segundo el tope y el tercero sera de cuanto en cuanto hara la incrementacion o decrementacion
+# empezara en 5, hasta <10 y en cada ciclo incrementara en 2 unidades
+for numero in range(5,10,2):
+    print(numero)
+
+
+# imprimir los 3 valores iniciales de notas
+notas = [10, 20, 16, 8, 6, 1]
+# AYUDITA: utilizar range(...)
+# AYUDITA: notas[..:..]
+for nota in notas[:3]:
+    print(nota)
+
+for posicion in range(3):
+    print(notas[posicion])
