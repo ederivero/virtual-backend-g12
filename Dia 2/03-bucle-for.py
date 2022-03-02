a172a9450311a4d4c4fec50b7eb7c1c07d980270
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-03-01 22:41:47 -0500

    avance control de flujos

---
+++ Dia 2/03-bucle-for.py
@@ -0,0 +1,7 @@
+notas = [10, 20, 16, 8, 6, 1]
+
+# for(let i=0; i<10; i++){...}
+# en cada iteracion de la lista notas tendremos su valor y lo almacenaremos en una variable llamda nota
+# el mismo funcionamiento se da para cualquier coleccion de datos (lista, tupla, diccionario, conjunto)
+for nota in notas:
+    print(nota)
