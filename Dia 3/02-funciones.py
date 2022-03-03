7b3fe38b35560b758c5f5e5ea25261af54778665
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-03-02 22:05:07 -0500

    avance funciones con numero ilimitado de parametros

--- Dia 3/02-funciones.py
+++ Dia 3/02-funciones.py
@@ -59,4 +59,37 @@ registrar_productos('Cebolla', 5.30, True, 'Almacen nuevo mercado')
 registrar_productos(almacen='Almacen de la costa', nombre='Pescado tilapia', precio=2.50)
 
 
+# recibira un numero indeterminado de parametros en los cuales los almacenara en una tupla
+# puede recibir un numero indeterminado de parametros y estos pueden ser de diferentes tipos
+# se puede crear parametros normales PERO al final siempre va el parametro generico (el que recibira n cantidad de parametros)
+def alumnos(clase, *args):
+    print(args)
 
+    # if(len(args) and args[0] is not None):
+    #     print('si hay el valor del puerto')
+    print('la clase es:',clase)
+
+# def alumnos(*args):
+#     print(args)
+
+
+alumnos('grupo12', 'eduardo','nahia', 'pedro', 'mario','jean carlo')
+
+alumnos('frontend', 'eduardo','roxana', 'luis', 'joshua', 'danny')
+
+alumnos('juanito')
+
+alumnos('martha', 30, False, 'Juan', 1.5)
+
+# kwargs > keyword argument
+# si la funcion queremos recibir un numero ilimitado de argumentos pero estos deben de tener su nombre de parametro con su valor entonces usaremos los kwargs y estos se almacenaran en un diccionario
+def ingresarProducto(**kwargs):
+    print(kwargs)
+    if(kwargs.get('nombre')):
+        print('El usuario quiere agregar un producto con el nombre')
+    if(kwargs.get('cantidad')):
+        print('El usuario quiere ingresar la cantidad del producto')
+
+
+ingresarProducto(nombre='Manzana', precio=2.40, estado=True, pais_procedencia='Peru')
+ingresarProducto(tamanio='Grande', cantidad= 100, nombre= 'Pera de agua')
