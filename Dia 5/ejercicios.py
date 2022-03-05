cf806ad2b1f3c16a5a73eed0ab74b1fe86885e3e
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-03-04 19:20:35 -0500

    tarea semana 01

---
+++ Dia 5/ejercicios.py
@@ -0,0 +1,39 @@
+# para evitar el salto de linea en una impresion de pantalla print() podemos declarar un parametro end=''
+print('hola',end='*')
+print('estos son los ejercicios')
+# Escriba una funcion que le pida al usuario ingresar la altura y el ancho de un rectangulo y que lo dibuje usando *, ejemplo:
+# altura: 5
+# ancho: 4
+# Resultado:
+# ****
+# ****
+# ****
+# ****
+# ****
+# dibujar_rectangulo()
+
+# Escribir una funcion que nosotros le ingresemos el grosor de un octagono y que lo dibuje
+# Ejemplo:
+# Grosor: 5
+#       *****
+#      *******
+#     *********
+#    ***********
+#   *************
+#   *************
+#   *************
+#   *************
+#   *************
+#    ***********
+#     *********
+#      *******
+#       *****
+# dibujar_octagono()
+
+# Ingresar un numero entero y ese numero debe de llegar a 1 usando la serie de Collatz
+# si el numero es par, se divide entre dos
+# si el numero es impar, se multiplica por 3 y se suma 1
+# la serie termina cuando el numero es 1
+# Ejemplo 19
+# 19 58 29 88 44 22 11 34 17 52 26 13 40 20 10 5 16 8 4 2 1
+# serie_collatz()
