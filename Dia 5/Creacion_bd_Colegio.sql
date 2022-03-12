193dcfb15268521aee33fb72ffca94e6f9976782
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-03-11 23:03:56 -0500

    modificaciones creacion bd colegio

--- Dia 5/Creacion_bd_Colegio.sql
+++ Dia 5/Creacion_bd_Colegio.sql
@@ -11,12 +11,26 @@ USE colegios;
 
 CREATE TABLE IF NOT EXISTS alumnos(
 	id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre varchar(100) not null,
+    apellido_paterno varchar(45),
+    apellido_materno varchar(45),
+    correo varchar(45) unique ,
+    numero_emergencia varchar(20) not null
 );
 
 CREATE TABLE IF NOT EXISTS niveles(
 	id INT PRIMARY KEY AUTO_INCREMENT,
+    seccion varchar(45) not null,
+    ubicacion varchar(45),
+    nombre varchar(45) not null
 );
 
 CREATE TABLE IF NOT EXISTS alumnos_niveles(
 	id INT PRIMARY KEY AUTO_INCREMENT,
+    alumno_id int,
+    nivel_id int,
+    fecha_cursada year,
+    FOREIGN KEY(alumno_id) REFERENCES alumnos(ID),
+    FOREIGN KEY(nivel_id) REFERENCES niveles(ID)
 );
+
