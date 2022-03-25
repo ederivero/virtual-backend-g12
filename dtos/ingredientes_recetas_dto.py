e09c4f57baa915a284ae595fb000f7c26d1a5ab7
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-03-24 19:58:57 -0500

    limpieza de la rama

--- dtos/ingredientes_recetas_dto.py
+++
@@ -1,7 +0,0 @@
-from config import validador
-from models.ingredientes_recetas import IngredientesRecetas
-
-class IngredientesRecetasRequestDTO(validador.SQLAlchemyAutoSchema):
-    class Meta:
-        model = IngredientesRecetas
-        include_fk = True
--- dtos/paginacion_dto.py
+++
@@ -1,8 +0,0 @@
-from config import validador
-from marshmallow import fields
-
-
-class PaginacionRequestDTO(validador.Schema):
-    # load_default > sirve para que en el caso no tenga valor esa variable entonces su valor por defecto a la hora de hacer el load(cargar) sera el indicado, NOTA: tiene que ser el mismo tipo de dato que hemos definido
-    page = fields.Integer(required=False, load_default=1)
-    perPage = fields.Integer(required= False, load_default=10)
