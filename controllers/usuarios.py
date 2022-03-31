7437d0f2f893d5bc934525b1b146b271f9fdb981
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-03-30 21:43:26 -0500

    envio de correos sin sendgrid

--- controllers/usuarios.py
+++ controllers/usuarios.py
@@ -3,7 +3,11 @@ from dtos.registro_dto import RegistroDTO, UsuarioResponseDTO, LoginDTO
 from dtos.usuario_dto import ResetPasswordRequestDTO
 from models.usuarios import Usuario
 from config import conexion, sendgrid
-from sendgrid.helpers.mail import Email, To, Content, Mail
+from smtplib import SMTP
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from os import environ
+# from sendgrid.helpers.mail import Email, To, Content, Mail
 
 
 class RegistroController(Resource):
@@ -50,34 +54,105 @@ class LoginController(Resource):
 class ResetPasswordController(Resource):
     def post(self):
         body = request.get_json()
+        # ------------- UTILIZANDO LA LIBRERIA DE PYTHON DE MENSAJERIA ----------------
+        # creo una variable en la cual almacenare toda la informacion de mi correo (mensaje)
+        mensaje = MIMEMultipart()
+        email_emisor = environ.get('EMAIL_EMISOR')
+        print(email_emisor)
+        email_password = environ.get('EMAIL_PASSWORD')
         try:
             data = ResetPasswordRequestDTO().load(body)
             # validar si existe ese usuario en mi bd
             usuarioEncontrado = conexion.session.query(
                 Usuario).filter_by(correo=data.get('correo')).first()
             if usuarioEncontrado is not None:
-                # tengo que utilizar los correos verificados en sendgrid ya que si uso uno que no esta verificado entonces el correo nunca llegara
-                print(usuarioEncontrado.correo)
-                from_email = Email('ederiveroman@gmail.com')
-                to_email = To(usuarioEncontrado.correo)
-                subject = 'Reinicia tu contraseña del Monedero App'
-                content = Content(
-                    'text/plain', 'Hola, has solicitado el reinicio de tu contraseña, haz click en el siguiente link para cambiar, sino has sido tu ignora este mensaje: ....')
-                mail = Mail(from_email, to_email, subject, content)
-                envia_correo = sendgrid.client.mail.send.post(
-                    request_body=mail.get())
-                # el estado de la respuesta de sendgrid
-                print(envia_correo.status_code)
-                # el cuerpo de la respuesta de sendgrid
-                print(envia_correo.body)
-                # las cabeceras de la respuesta de sendgrid
-                print(envia_correo.headers)
+                # texto = "Hola, este es un mensaje de prueba."
+                mensaje['Subject'] = 'Reiniciar contraseña Monedero APP'
+                # si queremos un generador de correos con diseño : https://beefree.io/
+                html = '''
+                <html>
+                    <body>
+                        <p>
+                            Hola {}, has solicitado el reinicio de tu contraseña de tu cuenta {} ?
+                        </p>
+                        <p style="color: #ababab;">
+                            Si has sido tu, entonces dale click al siguiente enlace: 
+                            <b> <a href="{}/reset-password">link</a> </b> 
+                        </p>
+                        <p>
+                            Si no has sido tu entonces has caso omiso a este mensaje.
+                        </p>
+                        <br>
+                        <h3>
+                            Por favor no responder a este mensaje ya que es automatico.
+                        </h3>
+                    </body>
+                </html>
+                '''.format(usuarioEncontrado.nombre, usuarioEncontrado.correo, environ.get('URL_FRONT'))
+                # siempre que queremos agregar un HTML como texto del mensaje tiene que ir despues del texto ya que primero tratara de enviar el ultimo y si no puede enviara el anterior
+                # mensaje.attach(MIMEText(texto, 'plain'))
+                mensaje.attach(MIMEText(html, 'html'))
+
+                # inicio el envio del correo
+                #                   SERVIDOR      | PUERTO
+                # outlook > outlook.office365.com | 587
+                # hotmail > smtp.live.com         | 587
+                # gmail >   smtp.gmail.com        | 587
+                # icloud >  smtp.mail.me.com      | 587
+                # yahoo >   smtp.mail.yahoo.com   | 587
+                # para los correos de GMAIL https://myaccount.google.com/lesssecureapps
+                emisorSMTP = SMTP('smtp.gmail.com', 587)
+                emisorSMTP.starttls()
+                # se hace el login de mi servidor de correo
+                emisorSMTP.login(email_emisor, email_password)
+                # envio el correo
+                emisorSMTP.sendmail(
+                    from_addr=email_emisor,
+                    to_addrs=usuarioEncontrado.correo,
+                    msg=mensaje.as_string()
+                )
+                # finalizo la sesion de mi correo
+                emisorSMTP.quit()
+                print('Correo enviado exitosamente')
 
             return {
                 'message': 'Correo enviado exitosamente'
             }
         except Exception as e:
             return {
-                'message': 'Error al resetear la password',
+                'message': 'Error al enviar el correo',
                 'content': e.args
             }
+
+        # ---------------- UTILIZANDO SENDGRID -----------------
+        # try:
+        #     data = ResetPasswordRequestDTO().load(body)
+        #     # validar si existe ese usuario en mi bd
+        #     usuarioEncontrado = conexion.session.query(
+        #         Usuario).filter_by(correo=data.get('correo')).first()
+        #     if usuarioEncontrado is not None:
+        #         # tengo que utilizar los correos verificados en sendgrid ya que si uso uno que no esta verificado entonces el correo nunca llegara
+        #         print(usuarioEncontrado.correo)
+        #         from_email = Email('ederiveroman@gmail.com')
+        #         to_email = To(usuarioEncontrado.correo)
+        #         subject = 'Reinicia tu contraseña del Monedero App'
+        #         content = Content(
+        #             'text/plain', 'Hola, has solicitado el reinicio de tu contraseña, haz click en el siguiente link para cambiar, sino has sido tu ignora este mensaje: ....')
+        #         mail = Mail(from_email, to_email, subject, content)
+        #         envia_correo = sendgrid.client.mail.send.post(
+        #             request_body=mail.get())
+        #         # el estado de la respuesta de sendgrid
+        #         print(envia_correo.status_code)
+        #         # el cuerpo de la respuesta de sendgrid
+        #         print(envia_correo.body)
+        #         # las cabeceras de la respuesta de sendgrid
+        #         print(envia_correo.headers)
+
+        #     return {
+        #         'message': 'Correo enviado exitosamente'
+        #     }
+        # except Exception as e:
+        #     return {
+        #         'message': 'Error al resetear la password',
+        #         'content': e.args
+        #     }
