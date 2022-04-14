19ce60fe5bbcb067a8458c60fd62553ae0ca94a6
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-04-13 23:05:49 -0500

    configuracion JWT

--- autorizacion/urls.py
+++ autorizacion/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import RegistroUsuarioApiView
+# Esto me retornara dos tokens una de acceso y una de refresh
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
-    path('registro/', RegistroUsuarioApiView.as_view())
+    path('registro/', RegistroUsuarioApiView.as_view()),
+    path('inicio-sesion/', TokenObtainPairView.as_view())
 ]
--- restaurante/settings.py
+++ restaurante/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 from os import environ
 from dotenv import load_dotenv
@@ -136,3 +137,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # sirve para definir cuando modificamos el contenido del modelo auth_user indicar ahora a cual tiene que hacer caso
 AUTH_USER_MODEL = 'autorizacion.Usuario'
+
+# sirve para toda la configuracion de nuestro DjangoRestFramework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': {
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    }
+}
+
+# sirve para modificar las configuraciones iniciales de simplejwt
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1)
+}
