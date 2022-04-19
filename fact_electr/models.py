e2d773a7d009cbe3fa52040306e53adfe4acca5d
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-04-18 21:47:26 -0500

    finalizacion creacion modelos

--- fact_electr/models.py
+++ fact_electr/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+from autorizacion.models import Usuario
+from menu.models import Stock
+
+
+class Pedido(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateTimeField()
+    total = models.FloatField(null=False)
+    numeroDocumentoCliente = models.CharField(max_length=12)
+    tipoDocumentoCliente = models.CharField(
+        choices=(['RUC', 'RUC'], ['DNI', 'DNI']), max_length=5)
+    mesa = models.IntegerField(null=False)
+    propina = models.FloatField()
+
+    # comprobante = models.OneToOneField(
+    #     to=Comprobante, related_name='pedido', on_delete=models.CASCADE, db_column='comprobante_id')
+
+    # hacer la relacion de ForeignKey con el modelo Usuarios
+    usuarioId = models.ForeignKey(
+        to=Usuario, related_name='pedidos', db_column='usuario_id', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'pedidos'
 
 
 class Comprobante(models.Model):
@@ -11,22 +34,22 @@ class Comprobante(models.Model):
     tipo = models.CharField(choices=(['BOLETA', 'BOLETA'], [
                             'FACTURA', 'FACTURA']), null=False, max_length=10)
 
+    pedido = models.OneToOneField(
+        to=Pedido, related_name='comprobante', on_delete=models.CASCADE, db_column='pedido_id')
+
     class Meta:
         db_table = 'comprobantes'
         unique_together = [['serie', 'numero']]
 
 
-class Pedido(models.Model):
+class DetallePedido(models.Model):
     id = models.AutoField(primary_key=True)
-    fecha = models.DateTimeField()
-    total = models.FloatField(null=False)
-    numeroDocumentoCliente = models.CharField(max_length=12)
-    tipoDocumentoCliente = models.CharField(
-        choices=(['RUC', 'RUC'], ['DNI', 'DNI']), max_length=5)
-    mesa = models.IntegerField()
-    propina = models.FloatField()
+    cantidad = models.IntegerField(null=False)
 
-    comprobante = models.OneToOneField(
-        to=Comprobante, related_name='pedido', on_delete=models.CASCADE, db_column='comprobante_id')
+    stockId = models.ForeignKey(
+        to=Stock, related_name='detalle_pedidos', on_delete=models.CASCADE, db_column='stock_id')
+    pedidoId = models.ForeignKey(
+        to=Pedido, related_name='detalle_pedidos', on_delete=models.CASCADE, db_column='pedido_id')
 
-    # hacer la relacion de ForeignKey con el modelo Usuarios
+    class Meta:
+        db_table = 'detalle_pedidos'
