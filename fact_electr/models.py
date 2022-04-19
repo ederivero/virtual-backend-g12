94d53ce0fd9bf1137311ab3daefa681115d31e24
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-04-18 20:38:32 -0500

    creacion de modelos faltantes

--- fact_electr/models.py
+++ fact_electr/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Comprobante(models.Model):
+    id = models.AutoField(primary_key=True)
+    serie = models.CharField(max_length=5, null=False)
+    numero = models.CharField(max_length=10, null=False)
+    pdf = models.TextField()
+    cdr = models.TextField()
+    xml = models.TextField()
+    tipo = models.CharField(choices=(['BOLETA', 'BOLETA'], [
+                            'FACTURA', 'FACTURA']), null=False, max_length=10)
+
+    class Meta:
+        db_table = 'comprobantes'
+        unique_together = [['serie', 'numero']]
+
+
+class Pedido(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateTimeField()
+    total = models.FloatField(null=False)
+    numeroDocumentoCliente = models.CharField(max_length=12)
+    tipoDocumentoCliente = models.CharField(
+        choices=(['RUC', 'RUC'], ['DNI', 'DNI']), max_length=5)
+    mesa = models.IntegerField()
+    propina = models.FloatField()
+
+    comprobante = models.OneToOneField(
+        to=Comprobante, related_name='pedido', on_delete=models.CASCADE, db_column='comprobante_id')
+
+    # hacer la relacion de ForeignKey con el modelo Usuarios
--- menu/models.py
+++ menu/models.py
@@ -12,3 +12,32 @@ class Plato(models.Model):
 
     class Meta:
         db_table = 'platos'
+
+
+class Stock(models.Model):
+    id = models.AutoField(primary_key=True)
+    fecha = models.DateField(null=False)
+    cantidad = models.IntegerField(null=False)
+    precio_diario = models.FloatField(null=False)
+
+    # related_name > servira para ingresar desde el modelo del cual se esta creando la relacion (en este caso desde platos podremos ingresar a todos sus stocks)
+    # on_delete > que accion tomara cuando se desee eliminar el padre (la PK)
+    #   CASCADE > eliminara el registro del plato y todos los stocks que tengan ese registro tbn seran eliminados en cascada
+    #   PROTECT > impedira que se realice la eliminacion del Plato si tiene stocks
+    #   SET_NULL > eliminara el plato y todos sus stocks colocara en su FK el valor de NULL
+    #   DO_NOTHING > eliminara el plato y no cambiara nada de los stocks (seguiran con el mismo valor ya eliminado)
+    #   RESTRICT > no permite la eliminacion y lanzara un error de tipo RestrictedError (hara un raise)
+    # https://docs.djangoproject.com/en/4.0/ref/models/fields/#django.db.models.ForeignKey.on_delete
+    platoId = models.ForeignKey(
+        to=Plato, related_name='stocks', on_delete=models.CASCADE, db_column='plato_id')
+
+    class Meta:
+        db_table = 'stocks'
+        # unique_together > crea un indice de dos o mas columnas en el cual no se podran repetir los mismos valores de esas columnas
+        # https://docs.djangoproject.com/en/4.0/ref/models/options/#unique-together
+        #   fecha    | plato_id
+        # 2022-04-18 |   1      ✅
+        # 2022-04-18 |   1      ❌
+        # 2022-04-18 |   2      ✅
+        # 2022-04-19 |   1      ✅
+        unique_together = [['fecha', 'platoId']]
