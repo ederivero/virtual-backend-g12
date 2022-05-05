8bbb23c2d8a8917edf0ad82858d195c325d59daf
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2022-05-04 22:45:16 -0500

    limpieza de la rama

--- CRUD_express/frontend/index.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "http://localhost:3000";
-
-async function pedirProductos() {
-  const respuesta = await fetch(`${BASE_URL}/productos`, { method: "GET" });
-  const data = await respuesta.json();
-  console.log(data);
-}
-
-pedirProductos();
--- minimarket/src/prisma.js
+++
@@ -1,5 +0,0 @@
-import prisma from "@prisma/client";
-
-const { PrismaClient } = prisma;
-
-export const Prisma = new PrismaClient();
